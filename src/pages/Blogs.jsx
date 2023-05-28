2e9f0f66d018de0e37373684b3d779f653d7deb4
iamansingh0 <amankumar7355197337@gmail.com>
2023-05-29 00:18:25 +0530

    commit

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import { blogs } from '../data/data'
-import '../css/blogs.css'
-import Background from '../components/Background'
+import React from "react"
+import Nav from "../components/Nav"
+import { blogs } from "../data/data"
+import "../css/blogs.css"
+import Background from "../components/Background"
 
 const Blogs = () => {
-  return (
-    <div className="blogs-div">
-      <Nav />
-      <Background />
-      <div className="blogs-container">
-        <div className="article-text-div">
-          <h4 className="article">ALL ARTICLES</h4>
-          <div className="line"></div>
+    const { image } = blogs[0]
+    return (
+        <div className="blogs-div">
+            <Nav />
+            <Background />
+            <div className="blogs-container">
+                <div className="article-text-div">
+                    <h4 className="article">ALL ARTICLES</h4>
+                    <div className="line"></div>
+                </div>
+                <article className="blog">
+                    <div className="text-left"></div>
+                    <div className="img-right">
+                        <div className="img">
+                            <img src={image} alt="" />
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <div className="buffer"></div>
         </div>
-        {blogs.map((blog) => {
-          const { id, title, image, date, time, brief } = blog;
-          return (
-            <article key={id} className="blog">
-              <h2 className="title"> </h2>
+    )
+}
 
-              {/* <hr style={{ color: "white", width: "3px" }} /> */}
-            </article>
-          );
-        })}
-      </div>
-      <div className="buffer"></div>
-    </div>
-  );
-};
-
-export default Blogs;
-
+export default Blogs
