a89f5253f82dbe315e6b000a3ee4f4f5d2a9a3d9
iamansingh0 <amankumar7355197337@gmail.com>
2023-05-30 22:52:43 +0530

    commit

--- src/data/data.js
+++ src/data/data.js
@@ -29,11 +29,8 @@ export const blogs = [
 ]
 
 export const socialLinks = [
-    {
-        id: 1,
-        href: "https://www.instagram.com/iaman._1/",
-        icon: "fab fa-instagram",
-    },
+    { id: 1, href: "https://www.instagram.com/iaman._1/", icon: "fab fa-instagram", },
     { id: 2, href: "https://twitter.com/dank_aman", icon: "fab fa-twitter" },
     { id: 3, href: "https://github.com/iamansingh0", icon: "fab fa-github" },
+    { id: 4, href: "https://www.linkedin.com/in/aman-kumar-singh-08b2b220b/", icon: "fab fa-linkedin" },
 ]
