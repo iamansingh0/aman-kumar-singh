3867d3bb3c6c23f8c7757f85740e06776a03ff47
iamansingh0 <amankumar7355197337@gmail.com>
2023-06-03 00:50:42 +0530

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import Nav from '../components/Nav'
+import React from "react"
+import Nav from "../components/Nav"
+import "../css/contact.css"
+import Background from "../components/Background"
 
 const Contact = () => {
-  return (
-    <div>
-      <Nav />
-      <h3>This is contact page</h3>
-    </div>
-  )
+    return (
+        <div>
+            <Nav />
+            <Background />
+            <div className="contact-container">
+                <div className="contact-links">
+                    <div className="contact-heading">
+                        <div className="heading-text wh-col">REACH OUT TO ME</div>
+                        <div className="heading-line"></div>
+                    </div>
+                    <div className="link-boxes">
+                        <div className="link-box"></div>
+                        <div className="link-box"></div>
+                        <div className="link-box"></div>
+                        <div className="link-box"></div>
+                    </div>
+                </div>
+                <div className="contact-form"></div>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
