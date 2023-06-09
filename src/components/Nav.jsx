48168d70f63d0c5170a20181f98368ce4d856c3d
iamansingh0 <amankumar7355197337@gmail.com>
2023-06-09 14:38:51 +0530

    commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,69 +3,69 @@ import { Link, useLocation } from "react-router-dom"
 import "../css/nav.css"
 
 const Nav = () => {
-    const location = useLocation()
+	const location = useLocation()
 
-    return (
-        <div className="navbar">
-            <p className="logo">
-                <Link to="/" id="name">
-                    AMAN
-                </Link>
-            </p>
-            <nav className="links">
-                <li className="link-li">
-                    <Link
-                        to="/"
-                        className={`item ${
-                            location.pathname === "/" ? "active" : ""
-                        }`}
-                    >
-                        Home
-                    </Link>
-                </li>
-                <li className="link-li">
-                    <Link
-                        to="/blogs"
-                        className={`item ${
-                            location.pathname === "/blogs" ? "active" : ""
-                        }`}
-                    >
-                        Blogs
-                    </Link>
-                </li>
-                <li className="link-li">
-                    <Link
-                        to="/about"
-                        className={`item ${
-                            location.pathname === "/about" ? "active" : ""
-                        }`}
-                    >
-                        About
-                    </Link>
-                </li>
-                <li className="link-li">
-                    <Link
-                        to="/contact"
-                        className={`item ${
-                            location.pathname === "/contact" ? "active" : ""
-                        }`}
-                    >
-                        Contact
-                    </Link>
-                </li>
-                <li className="link-li">
-                    <Link
-                        to="/projects"
-                        className={`item ${
-                            location.pathname === "/projects" ? "active" : ""
-                        }`}
-                    >
-                        Projects
-                    </Link>
-                </li>
-            </nav>
-        </div>
-    )
+	return (
+		<div className="navbar">
+			<p className="logo">
+				<Link to="/" id="name">
+					AMAN
+				</Link>
+			</p>
+			<nav className="links">
+				<li className="link-li">
+					<Link
+						to="/"
+						className={`item ${
+							location.pathname === "/" ? "active" : ""
+						}`}
+					>
+						Home
+					</Link>
+				</li>
+				<li className="link-li">
+					<Link
+						to="/blogs"
+						className={`item ${
+							location.pathname === "/blogs" ? "active" : ""
+						}`}
+					>
+						Blogs
+					</Link>
+				</li>
+				<li className="link-li">
+					<Link
+						to="/about"
+						className={`item ${
+							location.pathname === "/about" ? "active" : ""
+						}`}
+					>
+						About
+					</Link>
+				</li>
+				<li className="link-li">
+					<Link
+						to="/projects"
+						className={`item ${
+							location.pathname === "/projects" ? "active" : ""
+						}`}
+					>
+						Projects
+					</Link>
+				</li>
+				<li className="link-li">
+					<Link
+						to="/contact"
+						className={`item ${
+							location.pathname === "/contact" ? "active" : ""
+						}`}
+					>
+						Contact
+					</Link>
+				</li>
+			</nav>
+		</div>
+	)
 }
 
 export default Nav
